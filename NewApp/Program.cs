e8e47155a754f19a99dd69c6a4eda0f5e5d9409d
wiolettaflorkiewicz <wwflorkiewicz@gmail.com>
2024-03-19 14:14:17 +0100

    Modyfikacja 2

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,3 +3,11 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine($"Aktualna data i godzina: {DateTime.Now}");
+
+Console.WriteLine("Modyfikacja 2");
+static int Suma(int a, int b)
+{
+    return a + b;
+}
+
+Console.WriteLine($"Suma 3 + 4 = {Suma(3, 4)}");
